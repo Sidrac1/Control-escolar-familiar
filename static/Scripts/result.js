8a9af42160c0355ad984397f5f48b4cdabe13573
Sidrac1 <0323106010@ut-tijuana.edu.mx>
2025-11-13 14:02:35 -0800

    pruebas de conexion

--- static/Scripts/result.js
+++ static/Scripts/result.js
@@ -1,4 +1,24 @@
+async function cargarDatos() {
+    const domain = "https://control-escolar-familiar.onrender.com"
+    try{
+        const response = await fetch (`${domain}/lectura`);
+        
+        if(!response.ok) throw new Error("Error al obtener los datos");
+        const data = await response.json();
+        document.getElementById('resultado').textContent = JSON.stringify(data, null, 2);
+    }
+    catch (error){
+        document.getElementById('resultado').textContent = "Error: " + error.message;
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const testButton = document.getElementById('test_button');
+    if (testButton) {
+        testButton.addEventListener('click', cargarDatos);
+    }
+});
 
+/* 
 async function cargarDatosConFiltros(event) {
     if (event) event.preventDefault(); // Solo si viene de un submit
 
@@ -78,3 +98,4 @@ document.addEventListener("DOMContentLoaded", () => {
     // Carga inicial sin filtros
     cargarDatosConFiltros();
 });
+*/
